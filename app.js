78d830150df2b592a484e56c6586a21595516c77
Nicolas Urman <urmannicolas@gmail.com>
2020-07-15 15:55:09 +0200

    bodyparser added and handling errors added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const policies = require('./routes/policies');
 const app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -16,5 +18,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', auth);
 app.use('/policies', policies);
+
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'Not-Found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+  if (!res.headersSent) {
+    const statusError = err.status || '500';
+    res.status(statusError).json(err);
+  }
+});
 
 module.exports = app;
