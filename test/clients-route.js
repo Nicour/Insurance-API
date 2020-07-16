c659f6d1867b64f554512e65d0c5b10748895ad3
Nicolas Urman <urmannicolas@gmail.com>
2020-07-16 14:30:20 +0200

    tes clients route status 200

---
+++ test/clients-route.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const chai = require('chai');
+const should = require('chai').should();
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const assert = chai.assert;
+chai.use(chaiHttp);
+chai.should();
+
+const app = require('../app');
+const agent = chai.request.agent(app);
+
+describe("Test clients route", async () => {
+
+  it('Successful get all clients', (done) => {
+    agent
+    .get('/clients')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res.body).to.be.a('array')
+      expect(res.body[0]).to.have.property('email')
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+});
